b0241e05589fca378155ded5acc14851b8738b22
grodriguezm13 <guillirm1993@gmail.com>
2018-11-28 18:02:14 +0100

    Version 1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# practicaUT04.2
+# UT04.2 Video en Streaming
+
+## Introduction
+
+Autor - Guillermo Rodriguez Moraga
+
+Version 1.0.1 - Creacion de objetos en el archivo video.js
+- Se ha creado el objeto Person
+- Se ha creado el objeto Category
+- Se ha creado el objeto Resource
+- Se ha creado el objeto Production
+- Se ha creado el objeto Movie
+- Se ha creado el objeto Serie
+- Se ha creado el objeto Season
+- Se ha creado el objeto User
+- Se ha creado el objeto Coordinate
+
+Version 1.0.0 - Creacion del repositorio y archivos
+- Se ha creado el archivo video.html
+- Se ha creado el archivo video.js
---
+++ video.js
@@ -0,0 +1,548 @@
+"use strict";
+
+//Objeto para identificar los datos de una persona.
+function Person(name, lastName1, lastName2, born, picture) {
+    //Validación de parámetros obligatorios
+	name = typeof name !== 'undefined' ? name : "";
+	if (name === ""){ throw new Error("No puedes dejar el nombre vacio");}
+    lastName1 = typeof lastName1 !== 'undefined' ? lastName1 : "";
+    if (lastName1 === ""){ throw new Error("No puedes dejar el lastName vacio");}
+    born = typeof Date !== 'undefined' ? born : "";
+    if (born === ""){ throw new Error("El formato del nacimiento esta vacio o es invalido");}
+
+    //Declaracion de atributos
+    var _name = name;
+    var _lastName1 = lastName1;
+    var _lastName2 = lastName2 || "";
+    var _born = born;
+    var _picture = picture || "";
+
+    //Declaracion de getter y setters
+	Object.defineProperty(this, 'name', {
+		get:function(){
+			return _name;
+		},
+		set:function(value){
+			value = typeof name !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _name = value;
+            }//Fin del if
+        }		
+    });
+	
+	Object.defineProperty(this, 'lastName1', {
+		get:function(){
+			return _lastName1;
+		},
+		set:function(value){
+			value = typeof lastName1 !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el lastName1 vacio");
+            }else{
+                _lastName1 = value;
+            }//Fin del if
+        }		
+    });
+
+	Object.defineProperty(this, 'lastName2', {
+		get:function(){
+			return _lastName2;
+		},
+		set:function(value){
+			value = typeof lastName2 !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el lastName2 vacio");
+            }else{
+                _lastName2 = value;
+            }//Fin del if
+        }		
+	});
+	
+	Object.defineProperty(this, 'born', {
+		get:function(){
+			return _born;
+		},
+		set:function(value){
+			value = typeof Date !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la fecha de nacimiento vacia");
+            }else{
+                _born = value;
+            }//Fin del if
+        }		
+    });
+
+	Object.defineProperty(this, 'picture', {
+		get:function(){
+			return _picture;
+		},
+		set:function(value){
+			value = typeof picture !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la imagen vacia");
+            }else{
+                _picture = value;
+            }//Fin del if
+        }		
+    });
+}//Fin de Person
+Person.prototype.toString = function(){
+	return "Nombre completo: " + this.name + ", " + this.lastName1 + " " + this.lastName2 + ". Fecha de nacimiento: "+ this.born + ". Imagen: "+ this.picture;
+}
+
+//Con este objeto podemos crear la estructura de categorías.
+function Category(name, description) {
+	//Validación de parámetros obligatorios
+	name = typeof name !== 'undefined' ? name : "";
+	if (name === ""){ throw new Error("No puedes dejar el nombre vacio");}
+
+	//Declaracion de atributos
+    var _name = name;
+	var _description = description || "";
+	
+	//Declaracion de getter y setters
+	Object.defineProperty(this, 'name', {
+		get:function(){
+			return _name;
+		},
+		set:function(value){
+			value = typeof name !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _name = value;
+            }//Fin del if
+        }		
+    });
+	
+	Object.defineProperty(this, 'description', {
+		get:function(){
+			return _description;
+		},
+		set:function(value){
+			value = typeof description !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la descripcion vacia");
+            }else{
+                _description = value;
+            }//Fin del if
+        }		
+    });
+}//Fin de Category
+Category.prototype.toString = function(){
+	return "Nombre: " + this.name + ". Descripcion: "+ this.description;
+}
+
+//Representa un recurso audiovisual.
+function Resource(duration, link, audios, subtitles){
+	//Validación de parámetros obligatorios
+	duration = typeof duration !== 'undefined' ? duration : "";
+	if (duration === ""){ throw new Error("No puedes dejar la duracion vacia");}
+	link = typeof link !== 'undefined' ? link : "";
+	if (link === ""){ throw new Error("No puedes dejar la ruta vacia");}
+
+	//Declaracion de atributos
+    var _duration = duration;
+	var _link = link;
+	var _audios = audios || []; //Array de string
+	var _subtitles = subtitles || []; //Array de string
+
+	//Declaracion de getter y setters
+	Object.defineProperty(this, 'duration', {
+		get:function(){
+			return _duration;
+		},
+		set:function(value){
+			value = typeof duration !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _duration = value;
+            }//Fin del if
+        }		
+	});
+	
+	Object.defineProperty(this, 'link', {
+		get:function(){
+			return _link;
+		},
+		set:function(value){
+			value = typeof link !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _link = value;
+            }//Fin del if
+        }		
+	});
+
+	Object.defineProperty(this, 'audios', {
+		get:function(){
+			return _audios;
+		},
+		set:function(value){
+			value = typeof audios !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _audios = value;
+            }//Fin del if
+        }		
+	});
+
+	Object.defineProperty(this, 'subtitles', {
+		get:function(){
+			return _subtitles;
+		},
+		set:function(value){
+			value = typeof subtitles !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _subtitles = value;
+            }//Fin del if
+        }		
+	});
+}//Fin de Resource
+
+//Funcion para crear Production que tiene herencia con Movie y Serie
+(function(){ 
+    //Seguro para no instanciar objetos tipo Production
+	var abstractCreateLock = false; 
+
+	//Constructor de production.
+	function Production(title, nationality, publication, synopsis, image){
+		//Validación clase abstracta
+		if(abstractCreateLock){throw new Error("No puedes instanciar Production");}		
+
+		//Validación de parámetros obligatorios
+		title = typeof title !== 'undefined' ? title : "";
+		if (title === ""){ throw new Error("No puedes dejar el titulo vacio");}
+        publication = typeof Date !== 'undefined' ? publication : "";
+        if (publication === ""){ throw new Error("El formato de la publicacion esta vacio o es invalido");}
+        
+        //Declaracion de atributos
+        var _title = title; 
+        var _nationality = nationality;
+        var _publication = publication;
+        var _synopsis = synopsis;
+        var _image = image;
+
+		//Declaracion de getter y setters
+		Object.defineProperty(this, 'title', {
+			get:function(){
+				return _title;
+			},
+			set:function(value){
+				value = typeof title !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("No puedes dejar el titulo vacio");
+                }else{
+                    _title = value;
+                }//Fin del if
+			}		
+		});
+
+		Object.defineProperty(this, 'nationality', {
+			get:function(){
+				return _nationality;
+			},
+			set:function(value){
+				value = typeof nationality !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("No puedes dejar la nacionalidad vacia");
+                }else{
+                    _nationality = value;
+                }//Fin del if
+			}		
+		});
+
+		Object.defineProperty(this, 'publication', {
+			get:function(){
+				return _publication;
+			},
+			set:function(value){
+				value = typeof Date !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("No puedes dejar la fecha de publicacion vacia");
+                }else{
+                    _publication = value;
+                }//Fin del if
+			}		
+		});
+
+		Object.defineProperty(this, 'synopsis', {
+			get:function(){
+				return _synopsis;
+			},
+			set:function(value){
+				value = typeof synopsis !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("No puedes dejar la fecha de publicacion vacia");
+                }else{
+                    _synopsis = value;
+                }//Fin del if
+			}		
+		});
+
+		Object.defineProperty(this, 'image', {
+			get:function(){
+				return _image;
+			},
+			set:function(value){
+				value = typeof image !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("No puedes dejar la ruta de la imagen vacia");
+                }else{
+                    _image = value;
+                }//Fin del if
+			}		
+		});
+	}//Fin de  la funcion Production
+	Production.prototype = {}; 
+	Production.prototype.constructor = Production;
+	//Se define toString con los datos
+	Production.prototype.toString = function(){
+		return "Titulo: " + this.title + ". Nacionalidad: " + this.nationality + ". Fecha publicacion: " + this.publication + ". Sipnosis: " + this.synopsis + ". Imagen: " + this.image;
+	}
+
+	//Definimos la subclase MOvie que hereda de Production y Representa un recurso película que podremos reproducir en el sistema
+	function Movie(resource, locations){
+		//Llamada al superconstructor. Debemos desactivar el seguro para realizarla.
+		abstractCreateLock = false;
+		Production.call(this,resource, locations);	
+		abstractCreateLock = true;
+
+		//Declaracion de atributos
+		var _resource = resource || "";
+		var _locations = locations || "";
+
+		//Declaracion de getter y setters
+		Object.defineProperty(this, 'resource', {
+			get:function(){
+				return _resource;
+			},
+			set:function(value){
+				value = typeof Resource !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("Recurso invalido");
+                }else{
+                    _resource = value;
+                }//Fin del if
+			}		
+        });		
+        
+        Object.defineProperty(this, 'locations', {
+			get:function(){
+				return _locations;
+			},
+			set:function(value){
+				value = typeof Coordinate !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("Recurso invalido");
+                }else{
+                    _locations = value;
+                }//Fin del if
+			}		
+		});	
+    }//Fin de la funcion Movie
+    //Definimos la herencia
+	Movie.prototype = Object.create(Production.prototype); 
+	Movie.prototype.constructor = Movie;
+	Movie.prototype.toString = function(){
+		return Production.prototype.toString.call(this) + ". Recurso: " + this.resource + ". Coordenadas: " + this.locations;
+	}
+	
+	//Definimos la subclase serie que hereda de Production y representa un recurso serie que podremos reproducir
+	function Serie(season){
+		//Llamada al superconstructor. Debemos desactivar el seguro para realizarla.
+		abstractCreateLock = false;
+		Production.call(this,season);	
+		abstractCreateLock = true;
+
+		//Declaracion de atributos
+		var _season = season || "";
+
+		//Declaracion de getter y setters
+		Object.defineProperty(this, 'season', {
+			get:function(){
+				return _season;
+			},
+			set:function(value){
+				value = typeof Season !== 'undefined' ? value : "";
+		        if (value === ""){
+                    throw new Error("Recurso invalido");
+                }else{
+                    _season = value;
+                }//Fin del if
+			}		
+        });		
+    }//Fin de la funcion Serie
+    //Definimos la herencia
+	Serie.prototype = Object.create(Production.prototype); 
+	Serie.prototype.constructor = Serie;
+	Serie.prototype.toString = function(){
+		return Production.prototype.toString.call(this) + ". Temporadas: " + this.season;
+	}
+
+	abstractCreateLock = true; //Activamos el seguro
+
+	//Devolvemos constructores
+	window.Production = Production; 
+	window.Movie = Movie; 
+	window.Serie = Serie;  
+})();//Fin de la clase abstracta y las subclases
+
+//Definimos la clase season que representa una temporada de una serie.
+function Season(title, episodes){
+	//Validación de parámetros obligatorios
+	title = typeof title !== 'undefined' ? title : "";
+	if (title === ""){ throw new Error("No puedes dejar el titulo vacio");}
+
+    //Declaracion de atributos
+    var _title = title;
+    var _episodes = episodes || [];
+
+	//Declaracion de getter y setters
+	Object.defineProperty(this, 'title', {
+		get:function(){
+			return _title;
+		},
+		set:function(value){
+			value = typeof title !== 'undefined' ? value : "";
+			if (value === ""){
+				throw new Error("El titulo no puede dejarse vacio");
+			}else{
+				_title = value;
+			}//Fin del if
+		}		
+	});		
+
+	Object.defineProperty(this, 'episodes', {
+		get:function(){
+			return _episodes;
+		},
+		set:function(value){
+			value = typeof episodes !== 'undefined' ? value : "";
+			if (value === ""){
+				throw new Error("Episodio invalido");
+			}else{
+				_episodes = value;
+			}//Fin del if
+		}		
+	});	
+}//Fin de la funcion Serie
+Season.prototype.toString = function(){
+	return "Titulo: " + this.title + ". Episodios: "+ this.episodes;
+}
+
+//Objeto para identificar los datos de una persona.
+function User(userName, email, password) {
+    //Validación de parámetros obligatorios
+	userName = typeof userName !== 'undefined' ? userName : "";
+	if (userName === ""){ throw new Error("No puedes dejar el nombre vacio");}
+    email = typeof email !== 'undefined' ? email : "";
+    if (email === ""){ throw new Error("No puedes dejar el mail vacio");}
+    password = typeof password !== 'undefined' ? password : "";
+    if (password === ""){ throw new Error("No puedes dejar la contraseña vacia");}
+
+    //Declaracion de atributos
+    var _userName = userName;
+    var _email = email;
+    var _password = password;
+
+
+    //Declaracion de getter y setters
+	Object.defineProperty(this, 'userName', {
+		get:function(){
+			return _userName;
+		},
+		set:function(value){
+			value = typeof userName !== 'undefined' ? value : "";
+		    if (uvalue === ""){
+                throw new Error("No puedes dejar el nombre vacio");
+            }else{
+                _userName = value;
+            }//Fin del if
+        }		
+    });
+	
+	Object.defineProperty(this, 'email', {
+		get:function(){
+			return _email;
+		},
+		set:function(value){
+			value = typeof email !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar el mail vacio");
+            }else{
+                _email = value;
+            }//Fin del if
+        }		
+    });
+
+	Object.defineProperty(this, 'password', {
+		get:function(){
+			return _password;
+		},
+		set:function(value){
+			value = typeof password !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la contraseña vacia");
+            }else{
+                _password = value;
+            }//Fin del if
+        }		
+	});
+}//Fin de User
+User.prototype.toString = function(){
+	return "Nombre Usuario: " + this.userName + ". Email: "+ this.email + ". Pass: " + this.password;
+}
+
+//Objeto para identificar los datos de una persona.
+function Coordinate(latitude, longitude) {
+    //Validación de parámetros obligatorios
+	latitude = typeof latitude !== 'undefined' ? latitude : "";
+	if (latitude === ""){ throw new Error("No puedes dejar el nombre vacio");}
+    longitude = typeof longitude !== 'undefined' ? longitude : "";
+    if (longitude === ""){ throw new Error("No puedes dejar el mail vacio");}
+    
+    //Declaracion de atributos
+    var _latitude = latitude;
+    var _longitude = longitude;
+
+    //Declaracion de getter y setters
+	Object.defineProperty(this, 'latitude', {
+		get:function(){
+			return _latitude;
+		},
+		set:function(value){
+			value = typeof latitude !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la latitud vacia");
+            }else{
+                _latitude = value;
+            }//Fin del if
+        }		
+    });
+	
+	Object.defineProperty(this, 'longitude', {
+		get:function(){
+			return _longitude;
+		},
+		set:function(value){
+			value = typeof longitude !== 'undefined' ? value : "";
+		    if (value === ""){
+                throw new Error("No puedes dejar la longitud vacia");
+            }else{
+                _longitude = value;
+            }//Fin del if
+        }		
+    });
+}//Fin de Coordinate
+Coordinate.prototype.toString = function(){
+	return "Latitude: " + this.latitude + ". Longitud: "+ this.longitude;
+}
+
